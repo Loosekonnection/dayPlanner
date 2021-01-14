34669f203e5e383ac83025d004567c1f65303624
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-01-14 12:19:20 +0000

    store text in local storage completed

--- js/script.js
+++ js/script.js
@@ -5,32 +5,22 @@ var m = today.get("month");
 var d = today.get("day");
 var h = today.get("hour");
 
-// Console log date and time in different formats
-console.log("today's date and time: ", today.toISO());
-console.log("current month: ", m);
-console.log("current day: ", d);
-console.log("current hour: ", h);
-
 // Time variables
 var htmlLastHour = 17;
-var htmlHour = 11;
+var htmlHour = 12;
 var htmlHourSuffix = "";
 
-
-// Displays current date and/or time in header, uncomment which varient you want displayed
-// $("#currentDay").text(today.toLocaleString(dateTime.DATETIME_HUGE));
+// Displays current date and/or time in header
 $("#currentDay").text(today.toLocaleString(dateTime.DATE_HUGE));
-// $("#currentTime").text(today.toLocaleString(dateTime.TIME_SIMPLE));
 
 incrementHtmlHour();
 
 function incrementHtmlHour() {
 
     if (htmlHour >= 12) {
-        htmlHourSuffix = "PM"; 
+        htmlHourSuffix = "PM";
     } else if (htmlHour <= 11) {
-        htmlHourSuffix = "AM";     
-        
+        htmlHourSuffix = "AM";
     }
     createTimeBlocks();
 }
@@ -39,22 +29,22 @@ function incrementHtmlHour() {
 function createTimeBlocks() {
 
     var htmlTime = htmlHour + htmlHourSuffix;
-    
-        // Time block div
-        $timeBlock = $("<div>").addClass("row m-1");
-        // First child div conatining the displayed time
-        $displayTime = $("<div>").addClass("time-block hour").text(htmlTime);
-        $timeDiv = $("<div>").addClass("col-2 pr-0").append($displayTime);
-        // Next child div named textarea for inputting text
-        $textBlock = $("<textarea>").addClass("col-9 text").text("").attr("id", "data-colour");
-        // Next child div, the save icon at the end of the time block
-        $saveIcon = $("<i>").addClass("fas fa-save").attr("id", "data-save");
-        $saveDiv = $("<div>").addClass("col-1 saveBtn").append($saveIcon);
-        // this creates the time block div order of the above div's
-        $timeBlock.append($timeDiv, $textBlock, $saveDiv);
-        $("#scheduler").append($timeBlock);
 
-        textAreaBGColour();
+    // Time block div
+    $timeBlock = $("<div>").addClass("row m-1");
+    // First child div conatining the displayed time
+    $displayTime = $("<div>").addClass("time-block hour").text(htmlTime);
+    $timeDiv = $("<div>").addClass("col-2 pr-0").append($displayTime);
+    // Next child div named textarea for inputting text
+    $textBlock = $("<textarea>").addClass("col-9 text").text("").attr("id", "data-colour");
+    // Next child div, the save icon at the end of the time block
+    $saveIcon = $("<i>").addClass("fas fa-save").attr("id", "data-save");
+    $saveDiv = $("<div>").addClass("col-1 saveBtn").append($saveIcon);
+    // this creates the time block div order of the above div's
+    $timeBlock.append($timeDiv, $textBlock, $saveDiv);
+    $("#scheduler").append($timeBlock);
+
+    textAreaBGColour();
 }
 
 // Set the textarea background colour relative to the time of day
@@ -70,6 +60,33 @@ function textAreaBGColour() {
     } else {
         $("#data-colour").addClass("future");
     }
-
 }
+
+// Get text from local storage
+var textStore = JSON.parse(localStorage.getItem("textStore")) || [];
+
+// Store text in local storage
+$("#data-save").on("click", function (event) {
+    event.preventDefault();
+
+    var htmlTime = htmlHour + htmlHourSuffix;
+    var text = $(".text").val().trim();
+
+    // Validates the text field for text and alerts if none is present
+    if ($(".text").val() == '') {
+        alert("Please enter text.")
+        return;
+    } else {
+        text = {
+            time: htmlTime,
+            textInput: text
+        };
+        textStore.push(text);
+        localStorage.setItem("textStore", JSON.stringify(textStore));
+    }
+    getTextStored();
+});
+
+
+
 
