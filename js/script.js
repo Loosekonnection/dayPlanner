3898a17285b7502609c8fe44090c17d41ac48398
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-01-15 16:41:24 +0000

    Applied static HTML content, and just using JS for dynamic changes

--- js/script.js
+++ js/script.js
@@ -6,98 +6,54 @@ var d = today.get("day");
 var h = today.get("hour");
 
 // Time variables
-var htmlHourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var htmlHoursArray = [];
 var htmlHour = 9;
 var htmlHourSuffix = "";
 
 // Displays current date and/or time in header
 $("#currentDay").text(today.toLocaleString(dateTime.DATE_HUGE));
 
-setHtmlHour();
+textAreaBGColour();
 
-function setHtmlHour() {
-
-    if (htmlHour >= 12) {
-        htmlHourSuffix = "PM";
-    } else if (htmlHour <= 11) {
-        htmlHourSuffix = "AM";
-    }
-    createTimeBlocks();
-}
-
-// Create time blocks html dynamically
-function createTimeBlocks() {
-
-    var htmlTime = htmlHour + htmlHourSuffix;
-
-    // Time block div
-    $timeBlock = $("<div>").addClass("row m-1");
-    // First child div conatining the displayed time
-    $displayTime = $("<div>").addClass("time-block hour").text(htmlTime);
-    $timeDiv = $("<div>").addClass("col-2 pr-0").append($displayTime);
-    // Next child div named textarea for inputting text
-    $textBlock = $("<textarea>").addClass("col-9 text").text("").attr("id", "data-colour");
-    // Next child div, the save icon at the end of the time block
-    $saveIcon = $("<i>").addClass("fas fa-save").attr("id", "data-save");
-    $saveDiv = $("<div>").addClass("col-1 saveBtn").append($saveIcon);
-    // this creates the time block div order of the above div's
-    $timeBlock.append($timeDiv, $textBlock, $saveDiv);
-    $("#scheduler").append($timeBlock);
-
-    textAreaBGColour();
-    clearStorageButton();
-}
-
-// Set the textarea background colour relative to the time of day
 function textAreaBGColour() {
 
-    console.log("time: " + h);
-    console.log("textHour: ", htmlHour);
+    $("textarea").each(function () {
+        var htmlHours = $(this).attr("value");
+        htmlHoursArray.push(htmlHours);
+    });
 
-    if (htmlHour === h) {
-        $("#data-colour").addClass("present");
-    } else if (htmlHour < h) {
-        $("#data-colour").addClass("past");
-    } else {
-        $("#data-colour").addClass("future");
+    var currentHour = h.toString();
+
+    console.log("Text Area Value: ", $("textarea").attr("value"));
+    console.log("Current Hour: ", currentHour);
+    console.log("HTML Hours Array: ", htmlHoursArray);
+
+    for (var i = 0; i < htmlHoursArray.length; i++) {
+
+            // console.log(htmlHoursArray[i]);
+
+        if (currentHour === htmlHoursArray[i]) {
+            console.log("great its working");
+            $("textarea").addClass("present");
+
+        } else if (currentHour > htmlHoursArray[i]) {
+            
+
+        } else {
+            
+        }
     }
 }
 
-// Clear planner button
-function clearStorageButton() {
-
-    $clearDiv = $("<div>").addClass("row m-3");
-    $buttonDiv = $("<div>").addClass("col text-center");
-    $clearDiv.append($buttonDiv);
-
-    $($buttonDiv).append(
-        $(document.createElement('input')).prop({
-            type: 'button',
-            id: 'submit',
-            value: 'Clear Planner',
-            className: 'btn btn-secondary'
-        })
-    );
-    $("#scheduler").append($clearDiv);
-}
-
-// clear local storage & planner function
-$("#submit").on("click", function (event) {
-    event.preventDefault();
-
-    localStorage.clear();
-    window.location.reload();
-});
-
-// Get object from local storage
-var textStore = JSON.parse(localStorage.getItem("textStore")) || [];
-
 // Store text in local storage
-$("#data-save").on("click", function (event) {
+$(".fa-save").on("click", function (event) {
     event.preventDefault();
 
-    var htmlTime = htmlHour + htmlHourSuffix;
+    var pageTime = $(".time-block").attr("value");
     var text = $(".text").val().trim();
+
+    console.log("Page Time: ", pageTime);
+    console.log("Page Text: ", text);
 
     // Validates the text field for text and alerts if none is present
     if ($(".text").val() == '') {
@@ -105,24 +61,37 @@ $("#data-save").on("click", function (event) {
         return;
     } else {
         textEntered = {
-            time: htmlTime,
-            textInput: text
+            "time": pageTime,
+            "textInput": text
         };
-        textStore.push(textEntered);
-        localStorage.setItem("textStore", JSON.stringify(textStore));
+        var textCaptured = JSON.stringify(textEntered);
+        localStorage.setItem("timeBlockText", textCaptured);
     }
-    getStoredText();
 });
 
 // Get and display textarea from local storage
 function getStoredText() {
 
-    var htmlTime = htmlHour + htmlHourSuffix;
+    var storedText = localStorage.getItem("timeBlockText");
+    var text = JSON.parse(storedText);
 
-    console.log("Stored text: ", textStore);
-    
-    
+    var timeMatch = text.time;
+    var textSave = text.textInput;
+
+    if ($("textarea").attr("value") == timeMatch) {
+        $("textarea").val(textSave);
+    } else {
+        return;
+    }
 }
+
+// Clears local storage & planner function
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    localStorage.clear();
+    window.location.reload();
+});
 
 
 
