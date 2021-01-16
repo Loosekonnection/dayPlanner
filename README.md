4daf5baefe27e6d02153afce266f7765b7e7bc95
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-01-16 17:05:53 +0000

    Change plan to use dynamic HTML

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# dayPlanner
+# Work Day Plannerer
 Work Day Hourly Scheduler 
+
+*   GitHub Repository [JavaScript Coding Quiz](https://github.com/Loosekonnection/dayPlanner)
+*   GitHub Pages URL [Coding Quiz Webpage](https://loosekonnection.github.io/dayPlanner/)
+---
+
+<br>
+
+## Pseudo Code
+
+*   The current day and date has to be dynamically apended to the header.
+*   A mechnism to obtain the date and time is reuired.
+
+    *   for this I will use Luxon.
+
+*   hourly time slots are required with three columns:
+
+    *   An hourly time slot (starting at 9am).
+    *   A text input area to record user detail, task, meeting etc..
+    *   A save button to store the text input in local storage.
+
+*   The current time slot needs be idenfied with an individual color.
+*   The past time slot will need to change to grey.
+*   The future time slots will need to be green.
--- js/script.js
+++ js/script.js
@@ -6,52 +6,79 @@ var d = today.get("day");
 var h = today.get("hour");
 
 // Time variables
-var htmlHoursArray = [];
-var htmlHour = 9;
+var htmlHourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var textValArray = [];
 var htmlHourSuffix = "";
 
 // Displays current date and/or time in header
 $("#currentDay").text(today.toLocaleString(dateTime.DATE_HUGE));
 
-textAreaBGColour();
+createTimeBlocks();
+clearStorageButton();
 
+// Create time blocks html dynamically
+function createTimeBlocks() {
+
+    var i = 0;
+    while (i < htmlHourArray.length) {
+
+        if (htmlHourArray[i] >= 12) {
+            htmlHourSuffix = "pm";
+        } else {
+            htmlHourSuffix = "am";
+        }
+
+        var htmlTime = htmlHourArray[i]+ htmlHourSuffix;
+
+        // Time block div
+        $timeBlock = $("<div>").addClass("row m-1");
+        // First child div conatining the displayed time
+        $displayTime = $("<div>").addClass("time-block hour").text(htmlTime);
+        $timeDiv = $("<div>").addClass("col-2 pr-0").append($displayTime);
+        // Next child div named textarea for inputting text
+        $textBlock = $("<textarea>").addClass("col-9 text").text("").attr("value", htmlHourArray[i]);
+        // Next child div, the save icon at the end of the time block
+        $saveIcon = $("<i>").addClass("fas fa-save").attr("value", htmlHourArray[i]);
+        $saveDiv = $("<div>").addClass("col-1 saveBtn").append($saveIcon);
+        // this creates the time block div order of the above div's
+        $timeBlock.append($timeDiv, $textBlock, $saveDiv);
+        $("#scheduler").append($timeBlock);
+        
+        i++;
+     
+    }
+    textAreaBGColour();
+}
+
+// Set the textarea background colour relative to the time of day
 function textAreaBGColour() {
 
     $("textarea").each(function () {
         var htmlHours = $(this).attr("value");
-        htmlHoursArray.push(htmlHours);
+        textValArray.push(htmlHours);
     });
 
-    console.log("Text Area Value: ", $("textarea").attr("value"));
-    console.log("HTML Hours Array: ", htmlHoursArray);
+    for (var i = 0; i < textValArray.length; i++) {
 
-    for (var i = 0; i < htmlHoursArray.length; i++) {
-
-            // console.log(htmlHoursArray[i]);
-
-            if (parseInt(htmlHoursArray[i]) === h) {
-                $("textarea[value=" + htmlHoursArray[i] + "]").addClass("present");
-            } else if (parseInt(htmlHoursArray[i]) > h) {
-                $("textarea[value=" + htmlHoursArray[i] + "]").addClass("future");
+            if (parseInt(textValArray[i]) === h) {
+                $("textarea[value=" + textValArray[i] + "]").addClass("present");
+            } else if (parseInt(textValArray[i]) > h) {
+                $("textarea[value=" + textValArray[i] + "]").addClass("future");
             } else {
-                $("textarea[value=" + htmlHoursArray[i] + "]").addClass("past");
+                $("textarea[value=" + textValArray[i] + "]").addClass("past");
             }
     }
 }
 
+// Get object from local storage
+var textStore = JSON.parse(localStorage.getItem("textStore")) || [];
+
 // Store text in local storage
-$(".fa-save").on("click", function () {
-    
-    // $(".text").each(function () {
-    //     var saveBtnVal = $(this).attr("value");
-    //     saveBtnValArray.push(saveBtnVal);
-    // });
+$(".fa-save").on("click", "i", function (event) {
+    event.preventDefault();
 
-    var pageTime = $(".fa-save").attr("value");
+    var pageTime = $(this).attr("value");
     var text = $(".text").val().trim();
-
-    console.log("Page Time: ", pageTime);
-    console.log("Page Text: ", text);
 
     // Validates the text field for text and alerts if none is present
     if ($(".text").val() == '') {
@@ -59,11 +86,11 @@ $(".fa-save").on("click", function () {
         return;
     } else {
         textEntered = {
-            "time": pageTime,
-            "textInput": text
+            time: pageTime,
+            textInput: text
         };
-        var textCaptured = JSON.stringify(textEntered);
-        localStorage.setItem("timeBlockText", textCaptured);
+        textStore.push(textEntered);
+        localStorage.setItem("textStore", JSON.stringify(textStore));
     }
 });
 
@@ -76,16 +103,34 @@ function getStoredText() {
     var timeMatch = text.time;
     var textSave = text.textInput;
 
-    console.log
-
-    if ($("textarea").attr("value") == timeMatch) {
-        $("textarea").val(textSave);
-    } 
+    if ($(".time-block").text() == timeMatch) {
+        console.log("Woohoo! " + timeMatch);
+        $(".text").text(textSave);
+    } else {
+        $(".time-block").text();
+    }
 }
 
-// Clears local storage & planner function
-$("#submit").on("click", function (event) {
-    event.preventDefault();
+// Clear planner button
+function clearStorageButton() {
+
+    $clearDiv = $("<div>").addClass("row m-4");
+    $buttonDiv = $("<div>").addClass("col text-center");
+    $clearDiv.append($buttonDiv);
+
+    $($buttonDiv).append(
+        $(document.createElement('input')).prop({
+            type: 'button',
+            id: 'submit',
+            value: 'Clear Planner',
+            className: 'btn btn-secondary'
+        })
+    );
+    $("#scheduler").append($clearDiv);
+}
+
+// clear local storage & planner function
+$("#submit").on("click", function () {
 
     localStorage.clear();
     window.location.reload();
