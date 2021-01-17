ee06e5827a2e12c3cd439ad3af1bc7d653129c6f
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-01-17 20:30:02 +0000

    More getItem localstorage changes

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var h = today.get("hour");
 var htmlHourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var textValArray = [];
 var htmlHourSuffix = "";
+var timeArr = [];
 
 // Displays current date and/or time in header
 $("#currentDay").text(today.toLocaleString(dateTime.DATE_HUGE));
@@ -85,17 +86,47 @@ $(".fa-save").on("click", function (event) {
     var pageTime = $(this).attr("value");
     var text = $(this).parent().prev("textarea").val();
 
-    localStorage.setItem(pageTime, text);   
+    localStorage.setItem(pageTime, text);
    
 });
 
-var storedKey = localStorage.getItem("9");
-var textarea = $("<textarea>").attr("9");
+var storedKey9 = localStorage.getItem("9"); 
+var textarea9 = document.getElementByClassName(".text").attr("9");
+// textarea9.textContent = storedKey9;
+// document.getElementByClass(".text").value = "storedKey9";
+// $("textarea").attr("9").text("storedKey9");
+// document.getElementsByTagName("textarea").value = storedKey9;
+// textarea = storedKey9;
 
-textarea = storedKey;
-$("<textarea>").val(textarea);
+console.log(storedKey9);
 
-console.log(textarea);
+console.log(textarea9);
+
+
+
+var storedKey10 = localStorage.getItem("10");
+console.log(storedKey10);
+
+var storedKey11 = localStorage.getItem("11");
+console.log(storedKey11);
+
+var storedKey12 = localStorage.getItem("12");
+console.log(storedKey12);
+
+var storedKey13 = localStorage.getItem("13");
+console.log(storedKey13);
+
+var storedKey14 = localStorage.getItem("14");
+console.log(storedKey14);
+
+var storedKey15 = localStorage.getItem("15");
+console.log(storedKey15);
+
+var storedKey16 = localStorage.getItem("16");
+console.log(storedKey16);
+
+var storedKey17 = localStorage.getItem("17");
+console.log(storedKey17);
 
 
 // Clear planner button
